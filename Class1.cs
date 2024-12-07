c876a902fdc97ee3d9eeb9901aeeb63026368c77
Monica <mailto:mkrishnamurthy@uplandsoftware.com>
2024-12-07 13:44:00 +0530

    Add Selenium test class

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace YourNamespace
+{
+    [TestFixture]
+    public class SeleniumTestClass
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestGoogleSearch()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            var searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Jenkins");
+            searchBox.Submit();
+            Assert.IsTrue(driver.Title.Contains("Jenkins"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
